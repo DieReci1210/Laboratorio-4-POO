48a3561448f28d590ed0925e9ed75108162e6b9b
DieReci1210 <84409832+DieReci1210@users.noreply.github.com>
2022-11-08 23:00:23 -0600

    Add files via upload

---
+++ Interfaz.java
@@ -0,0 +1,19 @@
+public interface Funcionalidades {
+    public String viajes();
+    public String encenderApagar();
+    public String contactos();
+    public String llamar();
+    public String finalizarLLamada();
+    public String speaker();
+    public String listaReproduccion();
+    public String cancion();
+    public String escuchar();
+    public String cambiarModo();
+    public String emisoras();
+    public String guardarEmisoras();
+    public String cargarEmisora();
+    public String subirVolumen();
+    public String fswitch();
+    public String estado();
+    
+}
