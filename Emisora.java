686e5e20c12c673fd6cc75b5f8a1c0516d270e6f
DieReci1210 <84409832+DieReci1210@users.noreply.github.com>
2022-11-11 14:16:18 -0600

    Add files via upload

---
+++ Emisora.java
@@ -0,0 +1,20 @@
+public class Emisora{
+    private float numero;
+
+    
+
+    /**
+     * @return float return the numero
+     */
+    public float getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(float numero) {
+        this.numero = numero;
+    }
+
+}
